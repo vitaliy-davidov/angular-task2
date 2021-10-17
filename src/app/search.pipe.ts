08f5f4b20eab94f266cef0fa45124a34db7d5c0b
N0Test <prestyg42@gmail.com>
2021-10-17 19:36:10 +0300

    1 file modified

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -5,8 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
   transform(users: any[], value: any) {
-    console.log(value);
-
-    return users.filter(user => value == '' ? true : user.name.last.includes(value));
+    return users.filter(user => user.name.toLowerCase().includes(value.toLowerCase()));
   }
 }
